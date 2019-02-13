7b203c872994db67d3d1f60fb899cd2cac11dbfb
vinhub <vin@introp.net>
2019-02-12 17:30:55 -0800

    orientation

--- expt/main.js
+++ expt/main.js
@@ -9,16 +9,18 @@ const savePictureBtn = document.querySelector('#save-picture-button');
 const pictureImg = document.querySelector('#picture-img');
 
 takePictureBtn.onclick = video1.onclick = video2.onclick = function() {
+  const orientation = screen.msOrientation || (screen.orientation || screen.mozOrientation || {}).type;
+  const isLandscape = orientation.startsWith('landscape-');
   const canvas1 = document.createElement('canvas');
 
-  canvas1.width = pictureImg.style.width = video1.videoWidth * 2;
-  canvas1.height = pictureImg.style.height = video1.videoHeight;
+  canvas1.width = pictureImg.style.width = isLandscape ? video1.videoWidth * 2 : video1.videoWidth;
+  canvas1.height = pictureImg.style.height = isLandscape ? video1.videoHeight : video1.videoHeight * 2;
 
   const context = canvas1.getContext('2d');
   context.drawImage(video1, 0, 0);
   
-  const canvas2 = transformVideo(video1);
-  context.drawImage(canvas2, video1.videoWidth, 0);
+  const canvas2 = transformVideo(video1, isLandscape);
+  context.drawImage(canvas2, isLandscape ? video1.videoWidth : 0, isLandscape ? 0 : video1.videoHeight);
   canvas2.remove();
   
   // Other browsers will fall back to image/png
@@ -33,7 +35,7 @@ takePictureBtn.onclick = video1.onclick = video2.onclick = function() {
 
 savePictureBtn.onclick = function() {
   // Save picture
-  savePictureBtn.href = pictureImg.src.replace('image/png', 'image/octet-stream');
+  savePictureBtn.href = pictureImg.src; //.replace('image/png', 'image/octet-stream');
   savePictureBtn.download = 'caleido-' + Math.random().toString(36).substring(7) + '.png';
           
   pictureImg.style.display = 'none';
@@ -42,13 +44,13 @@ savePictureBtn.onclick = function() {
   takePictureBtn.disabled = false;
 };
 
-function transformVideo(video) {
+function transformVideo(video, isLandscape) {
   const canvas = document.createElement('canvas');
-  canvas.width = video1.videoWidth * 2;
-  canvas.height = video1.videoHeight;
+  canvas.width = isLandscape ? video.videoWidth * 2 : video.videoWidth;
+  canvas.height = isLandscape ? video.videoHeight : video.videoHeight * 2;
   const context = canvas.getContext('2d');
-  context.translate(video.videoWidth, 0);
-  context.scale(-1, 1);
+  context.translate(isLandscape ? video.videoWidth : 0, isLandscape ? 0 : video.videoHeight);
+  context.scale(isLandscape ? -1 : 1, isLandscape ? 1 : -1);
   context.drawImage(video, 0, 0);
   return canvas;
 }
